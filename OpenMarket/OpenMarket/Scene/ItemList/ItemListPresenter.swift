4892d9020774d61127cb0a387ac2df9c883d2729
blanche37 <wkddbsghk16@gmail.com>
2022-02-09 14:19:20 +0900

    feat: tableView구현

--- OpenMarket/OpenMarket/Scene/ItemList/ItemListPresenter.swift
+++ OpenMarket/OpenMarket/Scene/ItemList/ItemListPresenter.swift
@@ -5,14 +5,35 @@
 //  Created by yun on 2022/02/09.
 //
 
-import Foundation
+import UIKit
 
-protocol ItemListProtocol { }
+protocol ItemListProtocol {
+    func updateNavigation()
+    func updateView()
+}
 
-class ItemListPresenter {
+class ItemListPresenter: NSObject {
     private let viewController: ItemListProtocol
     
     init(viewController: ItemListProtocol) {
         self.viewController = viewController
     }
+    
+    func viewDidLoad() {
+        viewController.updateNavigation()
+        viewController.updateView()
+    }
+}
+
+extension ItemListPresenter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuse", for: indexPath)
+        cell.textLabel?.text = "테스트"
+        return cell
+    }
+    
 }
--- OpenMarket/OpenMarket/Scene/ItemList/ItemListViewController.swift
+++ OpenMarket/OpenMarket/Scene/ItemList/ItemListViewController.swift
@@ -5,16 +5,35 @@
 // 
 
 import UIKit
+import SnapKit
 
 class ItemListViewController: UIViewController {
     private lazy var presenter = ItemListPresenter(viewController: self)
-    
+
+    private lazy var tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.dataSource = presenter
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuse")
+        return tableView
+    }()
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter.viewDidLoad()
+        
     }
 }
 
 extension ItemListViewController: ItemListProtocol {
+    func updateNavigation() {
+        self.navigationItem.title = "야아 마켓"
+    }
     
+    func updateView() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(self.view)
+        }
+    }
 }
 
