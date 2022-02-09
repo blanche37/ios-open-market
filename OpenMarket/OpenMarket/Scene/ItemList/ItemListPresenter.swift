937525a36b8b066583abf0a7aa1414256e48f0f4
blanche37 <wkddbsghk16@gmail.com>
2022-02-09 12:48:27 +0900

    feat: 스토리보드 삭제후 초기세팅

---
+++ OpenMarket/OpenMarket/Scene/ItemList/ItemListPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  ItemListPresenter.swift
+//  OpenMarket
+//
+//  Created by yun on 2022/02/09.
+//
+
+import Foundation
--- OpenMarket/OpenMarket/Scene/ItemList/ItemListViewController.swift
+++ OpenMarket/OpenMarket/Scene/ItemList/ItemListViewController.swift
@@ -6,13 +6,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ItemListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
